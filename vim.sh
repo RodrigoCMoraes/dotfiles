95484538df0145caca9b1a99aa24f275839c1bf5
Rodrigo Moraes <rdcmdev@gmail.com>
2018-11-07 10:25:48 -0400

    fix: vim monokai theme
    Former-commit-id: 9937a3873f79f310c6ec159dcad91dd67dfae2c7

--- vim.sh
+++ vim.sh
@@ -2,7 +2,8 @@ sudo apt-get install vim vifm -y
 
 mkdir -p ~/.vim/colors/
 
-wget -c -P ~/.vim/colors https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
-wget -c -P ~/.config/vifm/colors/ https://raw.githubusercontent.com/vifm/vifm-colors/master/molokai.vifm
+wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim 
+wget https://raw.githubusercontent.com/vifm/vifm-colors/master/molokai.vifm
 
+cp monokai.vim ~/.vim/colors/
 cp -f vimrc ~/.vimrc
