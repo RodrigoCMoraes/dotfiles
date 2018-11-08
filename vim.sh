3c2c2649fefd8b0cf2036e462b73041b3f9f7928
Rodrigo Moraes <rdcmdev@gmail.com>
2018-11-08 10:25:23 -0400

    add vifm colorscheme
    Former-commit-id: 534023e2fddbaecbb87a176d2e6c0ac02adf6212

--- vim.sh
+++ vim.sh
@@ -1,9 +1,14 @@
 sudo apt-get install vim vifm -y
 
 mkdir -p ~/.vim/colors/
+mkdir -p ~/.vifm/colors/
 
 wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim 
-wget https://raw.githubusercontent.com/vifm/vifm-colors/master/molokai.vifm
+wget https://raw.githubusercontent.com/vifm/vifm/master/data/colors/istib-solarized-dark.vifm
 
-cp monokai.vim ~/.vim/colors/
+cp -f istib-solarized-dark.vifm ~/.vifm/colors/
+cp -f monokai.vim ~/.vim/colors/
+
 cp -f vimrc ~/.vimrc
+cp -f vifmrc ~/.config/vifm/
+cp -f vifmrc ~/.vifm/vifmrc
