136577b0181f83f356f7fbea2473bcd5a13d6600
RodrigoCMoraes <rdcmdev@gmail.com>
2019-02-13 21:43:08 -0400

    Update install.sh
    Former-commit-id: 13f14265c24eb9ae36be4b7040635d6b5196b8bc

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ apt-get install -y \
   thefuck \
   tmux
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd ~/.pc-setup
-cp -r .tmux.conf .bashrc .vim .vimrc .viminfo ~/
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
+cd ~/.pc-setup
+cp -r .tmux.conf .bashrc .vim .vimrc .viminfo ~/
