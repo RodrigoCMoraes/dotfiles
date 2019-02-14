c904b4ef25124278dd2567e72935655cc01ee736
RodrigoCMoraes <rdcmdev@gmail.com>
2019-02-13 21:26:37 -0400

    Update install.sh
    Former-commit-id: 2e33230ca170362e607b396395f503d52fbc1a6a

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 sudo apt-get update -y
-sudo apt-get install vim git tree -y
-git clone https://github.com/rodrigocmoraes/pc-setup.git ~/.pc-setup
+sudo apt-get install -y \
+  vim \
+  git \
+  tree \
+  thefuck \
+  tmux
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd ~/.pc-setup
 cp -r .tmux.conf .bashrc .vim .vimrc .viminfo ~/
